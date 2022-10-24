8c3b31501dbb7bc629e0867ca754a987349ebcf6
cymdiesel <cymantha.diesel@gmail.com>
2022-10-24 08:13:04 -0600

    Added Authgaurd to code

--- app/routes/contact.route.server.js
+++ app/routes/contact.route.server.js
@@ -1,14 +1,16 @@
 import {Router} from 'express';
 
+import { AuthGuard } from '../utils/index.js';
+
 import { DisplayContactAddPage, DisplayContactEditPage, DisplayContactList, ProcessContactAddPage, ProcessContactDelete, ProcessContactEditPage } from '../controllers/contact.controller.server.js';
 
 const router = Router();
 
 router.get('/contact-list', DisplayContactList);
-router.get('/contact-add', DisplayContactAddPage);
-router.post('/contact-add', ProcessContactAddPage);
-router.post('/contact-edit/:id', ProcessContactEditPage);
-router.get('/contact-edit/:id', DisplayContactEditPage);
-router.get('/contact-delete/:id', ProcessContactDelete);
+router.get('/contact-add', AuthGuard, DisplayContactAddPage);
+router.post('/contact-add', AuthGuard, ProcessContactAddPage);
+router.post('/contact-edit/:id', AuthGuard, ProcessContactEditPage);
+router.get('/contact-edit/:id', AuthGuard, DisplayContactEditPage);
+router.get('/contact-delete/:id', AuthGuard, ProcessContactDelete);
 
 export default router;
